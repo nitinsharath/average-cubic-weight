e80be403fd750b9e3e36c83dd0fce6855e904787
nitin <nitinsharath@gmail.com>
2020-08-01 10:26:33 +1000

    Add Product model

---
+++ models/product.py
@@ -0,0 +1,18 @@
+
+class Product:
+    CUBIC_WEIGHT_CONVERSION_FACTOR = 250  # Factor to convert cubic volume to weight
+
+    def __init__(self, category: str, width: float, length: float, height: float):
+        """ The width, length and height are assumed to be in cm """
+        self.category = category
+        self.width = width
+        self.length = length
+        self.height = height
+
+    def get_cubic_weight(self) -> float:
+        """ Returns cubic weight in kg """
+        return self._get_cubic_volume() * self.CUBIC_WEIGHT_CONVERSION_FACTOR
+
+    def _get_cubic_volume(self) -> float:
+        """ Returns cubic volume in m3 """
+        return self.width * self.length * self.height / 1000000
---
+++ models/test_product.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from models.product import Product
+
+
+class TestProduct(TestCase):
+    def setUp(self) -> None:
+        self.product_1 = Product(category='Cat 1', width=30, length=40, height=20)
+        self.product_2 = Product(category='Cat 2', width=49.5, length=56.7, height=87.5)
+        self.product_3 = Product(category='Cat 3', width=53.7, length=80, height=15.4)
+
+    def test_get_cubic_weight(self):
+        self.assertEqual(self.product_1.get_cubic_weight(), 6.0)
+        self.assertEqual(self.product_2.get_cubic_weight(), 61.39546875)
+        self.assertEqual(self.product_3.get_cubic_weight(), 16.5396)
